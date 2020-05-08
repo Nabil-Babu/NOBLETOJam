b0e24761c2f6a27fbc96c218d59c4388ba2655ff
Jamie Guillemette <jmguillemette@gmail.com>
2020-05-08 17:27:54 -0400

    Project Structure and Placeholders

---
+++ Assets/Noble/4.0 Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Noble {
+
+
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour {
+
+        public Vector2 moveInput = Vector2.zero;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate() {
+
+        }
+
+        public void OnMove(InputValue input) {
+            moveInput = input.Get<Vector2>();
+        }
+
+
+    }
+
+}

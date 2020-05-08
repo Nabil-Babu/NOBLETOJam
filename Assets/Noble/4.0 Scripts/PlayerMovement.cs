926448fe4e3106c3d41e9b52b9243c567a227ad7
Nabil Babu <nabil.babu20@gmail.com>
2020-05-08 17:44:52 -0400

    PlayerInput Testing

--- Assets/Noble/4.0 Scripts/PlayerMovement.cs
+++ Assets/Noble/4.0 Scripts/PlayerMovement.cs
@@ -7,18 +7,26 @@ namespace Noble {
 
 
     [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(PlayerInput))]
+
     public class PlayerMovement : MonoBehaviour {
 
         public Vector2 moveInput = Vector2.zero;
+        private CharacterController controller;
+
+        private Vector3 speed = Vector3.zero;  
 
         // Start is called before the first frame update
         void Start() {
-
+            speed = new Vector3(10,0,10);
+            controller = GetComponent<CharacterController>();
         }
 
         // Update is called once per frame
         void FixedUpdate() {
 
+
+            controller.Move(new Vector3(moveInput.x, 0, moveInput.y));
         }
 
         public void OnMove(InputValue input) {

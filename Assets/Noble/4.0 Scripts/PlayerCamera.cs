6dc77c7fedf661249ae2c5878493a857cdcb055b
Jamie Guillemette <jmguillemette@gmail.com>
2020-05-09 13:39:32 -0400

    Camera Improvements

--- Assets/Noble/4.0 Scripts/PlayerCamera.cs
+++ Assets/Noble/4.0 Scripts/PlayerCamera.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 
 public class PlayerCamera : MonoBehaviour {
 
+    [Header("Dependencies")]
+    public CinemachineVirtualCamera vCam;
+
+
     [Header("Settings")]
     public GameObject yawAxis;
-    public GameObject pitchAxis;
-    public float lookSpeed;
+    public float minHeight;
+    public float maxHeight;
+    public float yawSpeed;
+    public float pitchSpeed;
 
     [Header("Input Hooks")]
     public Vector2 lookInput = Vector2.zero;
@@ -18,22 +25,26 @@ public class PlayerCamera : MonoBehaviour {
     [SerializeField]
     private float yawAngle;
     [SerializeField]
-    private float pitchAngle;
+    private float camHeight;
+
     
-
-
     // Start is called before the first frame update
     void Start() {
-
+        
     }
 
     // Update is called once per frame
     void Update() {
-        yawAngle += lookInput.x * lookSpeed * Time.deltaTime;
+        yawAngle += lookInput.x * yawSpeed * Time.deltaTime;
         yawAxis.transform.localRotation = Quaternion.AngleAxis(yawAngle, Vector3.up);
+        camHeight += lookInput.y * pitchSpeed * Time.deltaTime;
+        camHeight = Mathf.Clamp(camHeight, minHeight, maxHeight);
+        CinemachineTransposer trans = vCam.GetCinemachineComponent<CinemachineTransposer>();
+        var offset = trans.m_FollowOffset;
+        offset.y = camHeight;
+        trans.m_FollowOffset = offset;
 
-        pitchAngle += lookInput.y * lookSpeed * Time.deltaTime;
-        pitchAxis.transform.localRotation = Quaternion.AngleAxis(pitchAngle, Vector3.right);
+
     }
 
     public void OnLook(InputValue input) {
---
+++ Assets/Noble/4.0 Scripts/PlayerPhysics.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerPhysics : MonoBehaviour {
+
+    public enum MODE {
+        None,
+        Direct,
+        ApplyForces
+    }
+
+    public MODE physicsMode;
+    public float directPower;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (!enabled) {
+            return;
+        }
+        if (physicsMode == MODE.None) {
+            return; // do nothing
+        }
+        var hitRB = hit.gameObject.GetComponentInParent<Rigidbody>();
+        if (hitRB == null || hitRB.isKinematic) {
+            return; // do nothing
+        }
+        CharacterController controller = hit.controller;
+        var footPos = controller.transform.position.y - controller.center.y - controller.height / 2;
+        if (hit.point.y <= footPos) {
+            // we are standing on it
+            return; // do nothing
+        }
+        if (physicsMode == MODE.ApplyForces) {
+            Debug.Log("Apply Forces");
+            hitRB.velocity = controller.velocity;
+
+        } else if (physicsMode == MODE.Direct) {
+            Debug.Log("Direct Velocity");
+            var force = controller.velocity * directPower;
+            hitRB.AddForceAtPosition(force, hit.point);
+        }
+
+
+    }
+
+}
+
